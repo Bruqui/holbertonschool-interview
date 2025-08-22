9da054d4c9b9d11bebe2c843b0f4b936594e9bbf
Bruqui <dylan2003gd@gmail.com>
2025-08-22 10:27:02 +0200

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -18,7 +18,6 @@ def canUnlockAll(boxes):
     n = len(boxes)
     visited = set()
     stack = [0]  # start with box 0
-
     while stack:
         current = stack.pop()
         if current not in visited:
@@ -26,5 +25,4 @@ def canUnlockAll(boxes):
             for key in boxes[current]:
                 if 0 <= key < n and key not in visited:
                     stack.append(key)
-                    
     return len(visited) == n
