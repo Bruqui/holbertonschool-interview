cbd9fa8fef1f14b3510e2a79bd8734078805895b
Bruqui <dylan2003gd@gmail.com>
2025-08-22 10:28:47 +0200

    blank lines

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -14,10 +14,12 @@ Each box may contain keys to other boxes.
   otherwise False.
 """
 
+
 def canUnlockAll(boxes):
     n = len(boxes)
     visited = set()
     stack = [0]  # start with box 0
+
     while stack:
         current = stack.pop()
         if current not in visited:
@@ -25,4 +27,5 @@ def canUnlockAll(boxes):
             for key in boxes[current]:
                 if 0 <= key < n and key not in visited:
                     stack.append(key)
+
     return len(visited) == n
