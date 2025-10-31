4c59dc2f851a785a09f792fecc5a9c4289531494
Bruqui <dylan2003gd@gmail.com>
2025-10-31 10:31:19 +0100

    Heap Insert

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+
 /**
  * heap_insert - Insère une valeur dans un arbre binaire max heap.
  * @root: Double pointeur vers la racine de l'arbre.
@@ -10,32 +11,32 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *new;     /* Créer un node vide nommée new*/
-    heap_t *current; /* Créer un node pour se déplacer*/
-    int n;           /* Variable qui prend le nombre de node à partir du nœud actuel */
+	heap_t *new;  /* Créer un node vide nommée new*/
+	heap_t *current;  /* Créer un node pour se déplacer*/
+	int n;  /* Variable qui prend le nombre de node à partir du nœud actuel */
 
-    current = *root; /* initialise current sur la racine de l'arbre */
+	current = *root;  /* initialise current sur la racine de l'arbre */
 
-    if ((*root) == NULL)
-    {
-        *root = createNode(value);
-        return (*root);
-    }
+	if ((*root) == NULL)
+	{
+		*root = createNode(value);
+		return (*root);
+	}
 
-    n = count_nodes(current) + 1;
-    new = createNode(value); /* alloue de la mémoire à new */
+	n = count_nodes(current) + 1;
+	new = createNode(value);  /* alloue de la mémoire à new */
 
-    new = find_and_attach_parent(current, new, n);
-    heap_t *result = new; /* save le nœud original */
-    /* cherche le nœud qui contient la valeur */
+	new = find_and_attach_parent(current, new, n);
+	heap_t *result = new;  /* save le nœud original */
+	/* cherche le nœud qui contient la valeur */
 
-    while (result->parent != NULL && value > result->parent->n)
-    {
-        result = result->parent;
-    }
+	while (result->parent != NULL && value > result->parent->n)
+	{
+		result = result->parent;
+	}
 
-    swap(new);
-    return (result);
+	swap(new);
+	return (result);
 }
 
 /**
@@ -48,42 +49,42 @@ heap_t *heap_insert(heap_t **root, int value)
  */
 heap_t *find_and_attach_parent(heap_t *current, heap_t *new, int n)
 {
-    int bits_size = 0; /* Nombre de bits nécessaires pour représenter n */
-    int temp = n;      /* variable temporaire de valeur n (nombre de nodes)*/
+	int bits_size = 0;  /* Nombre de bits nécessaires pour représenter n */
+	int temp = n;  /* variable temporaire de valeur n (nombre de nodes)*/
 
-    while (temp > 0)
-    {
-        bits_size++;
-        temp = temp >> 1;
-    }
+	while (temp > 0)
+	{
+		bits_size++;
+		temp = temp >> 1;
+	}
 
-    for (int i = bits_size - 2; i >= 1; i--)
-    {
-        int bit = (n >> i) & 1; /* Récupère le i-ème bit de n */
+	for (int i = bits_size - 2; i >= 1; i--)
+	{
+		int bit = (n >> i) & 1;  /* Récupère le i-ème bit de n */
 
-        if (bit == 0)
-        {
-            current = current->left; /* Aller à gauche si bit = 0 */
-        }
-        if (bit == 1)
-        {
-            current = current->right; /* Aller à droite si bit = 1 */
-        }
-    }
-    int last_bit = (n >> 0) & 1;
+		if (bit == 0)
+		{
+			current = current->left;  /* Aller à gauche si bit = 0 */
+		}
+		if (bit == 1)
+		{
+			current = current->right; /* Aller à droite si bit = 1 */
+		}
+	}
+	int last_bit = (n >> 0) & 1;
 
-    if (last_bit == 0)
-    {
-        current->left = new;
-        new->parent = current;
-    }
-    else
-    {
-        current->right = new;
-        new->parent = current;
-    }
+	if (last_bit == 0)
+	{
+		current->left = new;
+		new->parent = current;
+	}
+	else
+	{
+		current->right = new;
+		new->parent = current;
+	}
 
-    return (new);
+	return (new);
 }
 
 /**
@@ -93,15 +94,15 @@ heap_t *find_and_attach_parent(heap_t *current, heap_t *new, int n)
  */
 int swap(heap_t *new)
 {
-    while (new->parent != NULL && new->n > new->parent->n)
-    {
-        int temp = new->n;
+	while (new->parent != NULL && new->n > new->parent->n)
+	{
+		int temp = new->n;
 
-        new->n = new->parent->n;
-        new->parent->n = temp;
-        new = new->parent;
-    }
-    return (1);
+		new->n = new->parent->n;
+		new->parent->n = temp;
+		new = new->parent;
+	}
+	return (1);
 }
 
 /**
@@ -111,16 +112,16 @@ int swap(heap_t *new)
  */
 heap_t *createNode(int value)
 {
-    heap_t *new;
+	heap_t *new;
 
-    new = malloc(sizeof(heap_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = value;
-    new->parent = NULL;
-    new->left = NULL;
-    new->right = NULL;
-    return (new);
+	new = malloc(sizeof(heap_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = NULL;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
 }
 
 /**
@@ -130,7 +131,7 @@ heap_t *createNode(int value)
  */
 int count_nodes(heap_t *node)
 {
-    if (node == NULL)
-        return (0);
-    return (1 + count_nodes(node->left) + count_nodes(node->right));
+	if (node == NULL)
+		return (0);
+	return (1 + count_nodes(node->left) + count_nodes(node->right));
 }
